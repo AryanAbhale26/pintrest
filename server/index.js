2a79d1c77dfc687542cc4abf6bb63fb8a9c87fda
AryanAbhale162 <157355442+AryanAbhale26@users.noreply.github.com>
2025-07-29 15:09:59 +0530

    f

--- server/index.js
+++ server/index.js
@@ -5,29 +5,34 @@ import userRouter from "./routes/user.route.js";
 import boardRouter from "./routes/board.route.js";
 import commentRouter from "./routes/comment.route.js";
 import pinRouter from "./routes/pin.route.js";
-import mongoose from "mongoose";
 import { connectDB } from "./utils/connectDB.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 
 const app = express();
-const port = process.env.PORT;
+
+const port = process.env.PORT || 3000;
+
+// Middlewares
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
 app.use(cookieParser());
 app.use(fileUpload());
+
+// Routes
 app.use("/users", userRouter);
 app.use("/comments", commentRouter);
 app.use("/pins", pinRouter);
 app.use("/boards", boardRouter);
 
+// Connect DB & start server
+connectDB();
 app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
-  connectDB();
+  console.log(`Server is running on port ${port}`);
 });
